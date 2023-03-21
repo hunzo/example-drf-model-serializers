f5670b4e602e82368558a7d969bdf26e7568f18a
hunzo <takraw_s@hotmail.com>
2023-03-21 15:58:40 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"),
-    path('<int:pk>/', views.Home.as_view(), name="home")
+    path('', views.Home.as_view()),
+    path('<int:pk>/', views.Home.as_view()),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,36 +1,39 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+
+
 from .serializers import PayloadSerializers
 from .models import Payload
 
 
 class Home(APIView):
 
-    def get(self, request, pk=None):
-        if pk:
-            all_payload = Payload.objects.filter(pk=pk)
-        else:
-            all_payload = Payload.objects.all()
-        serializer = PayloadSerializers(all_payload, many=True)
-        # print(serializer.data)
+    def get_object(self, pk=None):
+        try:
+            return Payload.objects.get(pk=pk)
+        except Payload.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=None, format=None):
+
+        if pk is None:
+            payload = Payload.objects.all()
+            serializer = PayloadSerializers(payload, many=True)
+            return Response(serializer.data)
+
+        payload = self.get_object(pk=pk)
+        serializer = PayloadSerializers(payload)
+
         return Response(serializer.data)
 
     def post(self, request, pk=None):
-        if pk == None:
-            return Response({"test": "test"})
-        
-        serializer = PayloadSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        serializer = PayloadSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if pk is None:
+            serializer = PayloadSerializers(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
