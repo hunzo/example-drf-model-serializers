1fa084fee33bdeae7c5b72970d7b3ac464b577c6
hunzo <takraw_s@hotmail.com>
2023-03-22 12:54:25 +0700

    update

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from .serializers import PayloadSerializers
 from .models import Payload
 
+
 @api_view(["GET"])
 def GetPayloadView(request, pk=None):
     print(pk)
@@ -22,6 +23,7 @@ def GetPayloadView(request, pk=None):
             raise Http404
 
     return Response(serializers.data)
+
 
 @api_view(["POST"])
 def CreatePayload(request):
@@ -43,6 +45,7 @@ def UpdatePayload(request, pk):
 
     return Response(serializes.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(["DELETE"])
 def DeletePayload(request, pk):
     try:
@@ -51,57 +54,3 @@ def DeletePayload(request, pk):
         raise Http404
     payload.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-# class Home(APIView):
-#     """
-#     View to list all users in the system.
-#     # test
-#     ## test
-
-#     * Requires token authentication.
-#     * Only admin users are able to access this view.
-#     """
-
-#     def get_object(self, pk=None):
-#         try:
-#             return Payload.objects.get(pk=pk)
-#         except Payload.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk=None, format=None):
-#         """
-#         Return a list of all users.
-#         """
-#         if pk is None:
-#             payload = Payload.objects.all()
-#             serializer = PayloadSerializers(payload, many=True)
-#             return Response(serializer.data)
-
-#         payload = self.get_object(pk=pk)
-#         serializer = PayloadSerializers(payload)
-
-#         return Response(serializer.data)
-
-#     def post(self, request, pk=None):
-#         if pk is None:
-#             serializer = PayloadSerializers(data=request.data)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#         serializer = PayloadSerializers(data=request.data)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def put(self, request, pk=None):
-#         if pk is None:
-#             serializer = PayloadSerializers(data=request.data)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
